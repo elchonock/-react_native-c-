2e3326b60ac632678fd0742ca99c1af867d6e26d
elchonock <elya3029@gmail.com>
2025-05-18 19:49:31 +0200

    fix: use new components

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { GoalsPage } from './pages/goals-page';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Hello💜</Text>
-      <Text>🌸</Text>
       <StatusBar style="auto" />
+      <GoalsPage />
     </View>
   );
 }
@@ -15,8 +14,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
 });
